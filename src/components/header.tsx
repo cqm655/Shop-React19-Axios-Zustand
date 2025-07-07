ccf0ca1bbdb957e352ba5c1f08b6190add0a4e95
cqm655 <b.iura1992@gmail.com>
2025-07-07 16:11:57 +0300

    added add to card button added cart icon in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -38,6 +38,32 @@ export const Header = () => {
           Products
         </NavLink>
       </nav>
+      <div
+        className={`shopping-cart absolute top-1/3 right-[-80%] p-3 rounded-2xl hover:bg-gray-400 hover:cursor-pointer ${
+          scrolled
+            ? "bg-black/30 text-white"
+            : "bg-white/30 text-black dark:text-white/30"
+        }transition-all duration-400`}
+      >
+        <span className="absolute ml-[-25px] mt-[-10px] bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">
+          10
+        </span>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+          />
+        </svg>
+      </div>
     </header>
   );
 };
--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -61,37 +61,55 @@ export const ProductCard = ({ data, loading }: ProductCardProps) => {
             ></div>
           </div>
         </div>
-        <div className="rating-container">
-          <div className="flex self-start flex-1  w-full">
-            <span className="flex flex-1">
-              Rating:{" "}
-              <div className="flex flex-1 items-center">
-                <svg
-                  className="w-4 h-4 text-yellow-300 me-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <p className="ms-2 text-sm font-bold text-gray-900 dark:text-white">
-                  {data.rating}
-                </p>
-                <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                <a
-                  href="#"
-                  className="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
-                >
-                  {data.reviews.length}
-                </a>
-              </div>
-            </span>
+        <div className="grid  grid-rows-2 grid-flow-col">
+          <div className="rating-container ">
+            <div className="flex self-start flex-1  w-full">
+              <span className="flex flex-1">
+                Rating:{" "}
+                <div className="flex flex-1 items-center">
+                  <svg
+                    className="w-4 h-4 text-yellow-300 me-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <p className="ms-2 text-sm font-bold text-gray-900 dark:text-white">
+                    {data.rating}
+                  </p>
+                  <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
+                  >
+                    {data.reviews.length}
+                  </a>
+                </div>
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="price-conainer">
-          <div className="flex self-start items-center flex-1 w-full space-between">
-            <span className="font-bold ">Price: {data.price}</span>
+          <div className="price-conainer">
+            <div className="flex self-start items-center flex-1 w-full space-between">
+              <span className="font-bold ">Price: {data.price}</span>
+            </div>
+          </div>
+          <div className="add-to_cart col-span-2 row-span-2 self-center ml-8 p-3 hover:bg-gray-400 rounded-2xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
           </div>
         </div>
       </div>
